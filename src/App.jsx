09af48d6147b72fb461bd023e4af82bb92b61570
akhrorwork <akhrorwork@gmail.com>
2024-04-03 09:47:32 +0500

    test project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati vel
+        nihil tenetur rerum eos architecto delectus suscipit adipisci aut ut
+        porro consequatur ipsum error voluptate facere sequi dicta, at sit nobis
+        inventore sapiente placeat possimus! Id, libero. Ullam et architecto
+        facilis magnam nobis odit repellat quam, impedit veniam tempore.
+        Sapiente!
+      </p>
+      <button className="btn btn-primary">Click</button>
+    </>
+  );
+}
+
+export default App;
